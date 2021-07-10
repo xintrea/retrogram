0d9f6642cf521b0563512b0352a957e66635095f
xintrea <xintrea@gmail.com>
2021-07-10 03:23:37 +0300

    Сделан нормальный цифровой шум

--- shader.glsl
+++ shader.glsl
@@ -13,19 +13,45 @@ uniform sampler2D texPreviousFrame; // screenshot of the previous frame
 layout(location = 0) out vec4 out_color; // out_color must be written in order to see anything
 
 
-highp float rand(float seed)
-{
-    float shift=texture( texFFT, seed ).r;
-
-    vec2 co=vec2(fGlobalTime+shift, fGlobalTime-shift);
-    
-    highp float a = 12.9898;
-    highp float b = 78.233;
-    highp float c = 43758.5453;
-    highp float dt= dot(co.xy ,vec2(a,b));
-    highp float sn= mod(dt,3.14);
-    return fract(sin(sn) * c);
+// A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
+uint hash( uint x ) {
+    x += ( x << 10u );
+    x ^= ( x >>  6u );
+    x += ( x <<  3u );
+    x ^= ( x >> 11u );
+    x += ( x << 15u );
+    return x;
 }
+
+
+
+// Compound versions of the hashing algorithm I whipped together.
+uint hash( uvec2 v ) { return hash( v.x ^ hash(v.y)                         ); }
+uint hash( uvec3 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z)             ); }
+uint hash( uvec4 v ) { return hash( v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w) ); }
+
+
+
+// Construct a float with half-open range [0:1] using low 23 bits.
+// All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
+float floatConstruct( uint m ) {
+    const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
+    const uint ieeeOne      = 0x3F800000u; // 1.0 in IEEE binary32
+
+    m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
+    m |= ieeeOne;                          // Add fractional part to 1.0
+
+    float  f = uintBitsToFloat( m );       // Range [1:2]
+    return f - 1.0;                        // Range [0:1]
+}
+
+
+
+// Pseudo-random value in half-open range [0:1].
+float rand( float x ) { return floatConstruct(hash(floatBitsToUint(x+fGlobalTime))); }
+float rand( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float rand( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
+float rand( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 
 
 void main(void)
