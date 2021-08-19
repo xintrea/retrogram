086d4589ae58c5afb2221821a2034f36d9524eb2
xintrea <xintrea@gmail.com>
2021-08-19 19:03:22 +0300

    Добавлен консольный плейер, изменен конфиг

--- start_lin.sh
+++ start_lin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# export MESA_GL_VERSION_OVERRIDE=4.1
-# export MESA_GLSL_VERSION_OVERRIDE=410
-
-
 echo "Start music"
 vlc --intf dummy --play-and-exit ./sound/klaudia_blue_shawl.mp3 &
 
---
+++ start_win.bat
@@ -0,0 +1,10 @@
+
+echo "Start music"
+vlc --intf dummy --play-and-exit ./sound/klaudia_blue_shawl.mp3 &
+
+echo "Start Bonzomatic"
+./bonzomatic
+
+echo "Stop music"
+kill %1
+
