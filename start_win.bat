b2b3fe6d79982573f94c250505de8d9f4a5db168
xintrea <xintrea@gmail.com>
2021-08-25 00:36:14 +0300

    Обновлен бинарник для Linux, теперь он поддерживает выход по ESC

--- start_win.bat
+++ start_win.bat
@@ -1,9 +1,9 @@
 
 echo "Start music"
-start "play-klaudia-sound" /MIN .\sound\mpg123.exe .\sound\klaudia_blue_shawl.mp3
+start "play-sound" /MIN .\sound\mpg123.exe .\sound\klaudia_blue_shawl.mp3
 
 echo "Start Bonzomatic"
 .\bonzomatic
 
 echo "Stop music"
-taskkill /F /FI "WINDOWTITLE eq play-klaudia-sound"
+taskkill /F /FI "WINDOWTITLE eq play-sound"
